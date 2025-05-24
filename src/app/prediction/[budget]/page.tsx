6a316db0f5ad03df86b8f80fd2b77d85f001d0fe
Hai Jie <haijie95@gmail.com>
2025-05-24 17:26:31 +0800

    Minor change

--- src/app/prediction/[budget]/page.tsx
+++ src/app/prediction/[budget]/page.tsx
@@ -4,8 +4,9 @@ import { useParams } from "next/navigation";
 
 export default function Prediction() {
   const param = useParams<{ budget: string }>;
+  const { budget } = useParams<{ budget: string }>();
 
-  //   console.log(param.toString);
+  console.log("Budget:", budget);
 
   return (
     <div className="w-screen min-h-screen bg-gradient-to-b from-gray-700 via-gray-900 to-black">
@@ -16,8 +17,11 @@ export default function Prediction() {
               className="mr-3 text-5xl font-semibold"
               style={{ color: "white" }}
             >
-              Work In Progress!
+              Work In Progress! Hello 
             </h1>
+            <p className="text-white mt-4">
+              Budget: {budget} {/* Display the retrieved budget */}
+            </p>
           </div>
         </div>
       </div>
